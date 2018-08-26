dcd8c4fe08b213b02c09b8b606eeff109763728e
Philip Lu <philip.lu@wishabi.com>
2018-08-26 07:21:48 -0400

    report table generation

--- app/src/main/java/com/example/lance/ht6/utils/DatabaseUtilities.java
+++ app/src/main/java/com/example/lance/ht6/utils/DatabaseUtilities.java
@@ -2,6 +2,7 @@ package com.example.lance.ht6.utils;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.lance.ht6.schemas.EventsTableContract.EventsEntry;
@@ -11,12 +12,13 @@ import com.example.lance.ht6.schemas.CountsTableContract.CountsEntry;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 public class DatabaseUtilities {
 
-    DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm z");
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public void updateEvents(SQLiteDatabase dbEvents,
                              String text,
                              List<String> wordList) {
@@ -53,7 +55,41 @@ public class DatabaseUtilities {
                                       SQLiteDatabase dbReports,
                                       List<String> wordList,
                                       int sessionId) {
-        // update dbReports
+        // build query
+        String newRowQuery = "select datetime((strftime('%s', time) / 3600) * 3600, 'unixepoch') interval, word, count(*) as count " +
+                "from Events " +
+                "where session = ? " +
+                "GROUP BY interval, word " +
+                "ORDER BY interval";
+
+        // get new rows from events
+        Cursor newRows = dbEvents.rawQuery(newRowQuery, new String[]{ Integer.toString(sessionId) });
+        ContentValues newValues;
+        ArrayList<ContentValues> retVal = new ArrayList<ContentValues>();
+        if(newRows.moveToFirst()) {
+            do {
+                newValues = new ContentValues();
+                DatabaseUtils.cursorRowToContentValues(newRows, newValues);
+                retVal.add(newValues);
+            } while(newRows.moveToNext());
+        }
+
+        // update rows
+        String selection = ReportPerMinuteEntry.SESSION_COLUMN + " LIKE ?";
+        String[] selectionArgs = { Integer.toString(sessionId) };
+
+        dbReports.beginTransaction();
+
+        for (int i = 0; i < retVal.size(); i++) {
+            dbReports.update(
+                    ReportPerMinuteEntry.TABLE_NAME,
+                    retVal.get(i),
+                    selection,
+                    selectionArgs);
+        }
+
+        dbReports.setTransactionSuccessful();
+        dbReports.endTransaction();
     }
 
     public void resetTables(SQLiteDatabase dbEvents,
