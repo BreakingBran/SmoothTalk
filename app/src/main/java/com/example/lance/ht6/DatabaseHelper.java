478fa9291d0034d70e74c209191e55c901cdcdc6
alice-mu <alicemu99@gmail.com>
2018-08-26 04:45:46 -0400

    fix merge conflicts

--- app/src/main/java/com/example/lance/ht6/DatabaseHelper.java
+++ app/src/main/java/com/example/lance/ht6/DatabaseHelper.java
@@ -1,30 +1,48 @@
 package com.example.lance.ht6;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
 
     public static final String DATABASE_NAME = "words.db";
     public static final String TABLE_NAME = "words_table";
     public static final String COL_1 = "ID";
-    public static final String COL_2 = "words";
-    public static final String COL_3 = "count";
+    public static final String COL_2 = "WORDS";
+    public static final String COL_3 = "COUNT";
+    private static final String TAG = "DatabaseHelper";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, WORD TEXT, COUNT INTEGER)");
+        db.execSQL("create table words_table (ID INTEGER PRIMARY KEY AUTOINCREMENT, WORD TEXT,COUNT INTEGER)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertWordData(String word, Integer count){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("WORD",word);
+        contentValues.put("COUNT",count);
+        Log.d(TAG, "insertWordData: Called with word " + word + " and count " + count.toString());
+        
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
     }
 }
--- app/src/main/java/com/example/lance/ht6/NewWords.java
+++ app/src/main/java/com/example/lance/ht6/NewWords.java
@@ -17,6 +17,10 @@ public class NewWords extends AppCompatActivity {
     private static final String TAG = "NewWords";
     private static final String filename = "keywords.txt";
     private FileOutputStream outputStream;
+    Button btnNavToMain;
+    EditText wordField;
+    Button submitNewWord;
+    DatabaseHelper myDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +44,11 @@ public class NewWords extends AppCompatActivity {
                 e.printStackTrace();
             }
         });
+
+        myDb = new DatabaseHelper(this);
+
+        //        NAVIGATE TO MAIN PAGE
+        btnNavToMain = (Button) findViewById(R.id.newWordsToMainButton);
 
         btnSubmit.setOnClickListener((v) -> {
             String textToWrite = mEdit.getText().toString();
@@ -67,5 +76,28 @@ public class NewWords extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //        NEW WORD FIELD
+        wordField = (EditText) findViewById(R.id.new_word_field);
+
+        //      SUBMIT NEW WORD BUTTON
+        submitNewWord = (Button) findViewById(R.id.new_word_submit);
+
+        submitNewWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: attempted submited new word");
+                if(wordField.getText().toString() != ""){
+                    boolean isInserted = myDb.insertWordData(wordField.getText().toString(),0);
+                    if(isInserted == true){
+                        Toast.makeText(NewWords.this, "Word Added", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(NewWords.this, "Failed to Add Word :(", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
     }
 }
