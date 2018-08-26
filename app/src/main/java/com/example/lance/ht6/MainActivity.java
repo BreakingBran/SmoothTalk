29edeafcd1450bb9e7da13ffb59546e4757ed59b
BreakingBran <lancepereira99@gmail.com>
2018-08-26 00:32:16 -0400

    Im a fucking God I got the buttons working

--- app/src/main/java/com/example/lance/ht6/MainActivity.java
+++ app/src/main/java/com/example/lance/ht6/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.lance.ht6;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -11,18 +13,24 @@ public class MainActivity extends AppCompatActivity {
     ImageButton settingsButton;
     Button startButton;
     DatabaseHelper myDb;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d(TAG, "onCreate: Starting");
         settingsButton = findViewById(R.id.settings_button);
         settingsButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Clicked the go to create new words (settings) button");
+                Intent settingsIntent = new Intent(MainActivity.this,   NewWords.class);
+                startActivity(settingsIntent);
             }
         });
         startButton = findViewById(R.id.start_button);
         myDb = new DatabaseHelper(this);
     }
+
 }
