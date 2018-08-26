7a7ecb22cb2c1105d23ebe80b5f7a615f132385b
alice-mu <alicemu99@gmail.com>
2018-08-25 21:44:24 -0400

    added buttons and functionality to main page

--- app/src/main/java/com/example/lance/ht6/MainActivity.java
+++ app/src/main/java/com/example/lance/ht6/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.lance.ht6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton settingsButton;
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settingsButton = findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        startButton = findViewById(R.id.start_button);
     }
 }
